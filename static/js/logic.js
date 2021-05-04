57e7121399780066915beb0063d5152948e5519f
marlyng <marlyngmorales26@gmail.com>
2021-05-04 17:44:48 -0400

    finally working

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,106 +1,96 @@
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(data) {  
+  createFeatures(data.features);
+});
 
 
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level for sf
-// This gets inserted into the div with an id of 'map' in index.html
-var myMap = L.map("map", {
-    center: [40.7128, -74.0059],
-    zoom: 11
+// CREATE BASIC MAPS: LIGHT MAP AND SATELLITE MAP
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/light-v10",
+    accessToken: API_KEY
   });
-  
-  // Adding a tile layer (the background map image) to our map
-  // We use the addTo method to add objects to our map
-  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "satellite-v9",
     accessToken: API_KEY
-  }).addTo(myMap);
+  });
 
-  // Store our API endpoint
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Light Map": lightmap,
+    "Satellite": satellitemap
+  };
 
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_hour.geojson";
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
 
-//  GET color radius call to the query URL
-d3.json(url, function(data) {
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "black",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
+  // Create myMap, giving it the lightmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [lightmap, earthquakes]
+  });
+
+  // Create a layer control
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  })    
+  .addTo(myMap);
+}
+
+// CREATE EARTHQUAKE FEATURES
+function createFeatures(earthquakeData) {
+  
+  // For each feature create Popup leyend, size and color the marker
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3> Magnitude: " + feature.properties.mag +
+      "</h3><hr><h3> Location: " + (feature.properties.place) + "</h3><hr><h3> Depth: "
+      + (feature.geometry.coordinates[2])+ "</h3>");
+
+      var magnitude = feature.properties.mag
+      var depth = feature.geometry.coordinates[2]
+      var longitude = feature.geometry.coordinates[0]
+      var latitude = feature.geometry.coordinates[1]      
+
+    // Conditional coloring of circles based on depth
+      var color = "";
+      if(depth > 25){ color = "red";}
+      else if (depth > 15){ color = "yellow";}
+      else{ color = "green";}
+
+    // Size the market based on the magnitude of the earthquake
+    var markerOptions = {
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: color,
+      radius : magnitude * 200
     };
-  }
-  // set different color from magnitude
-    function getColor(magnitude) {
-    switch (true) {
-    case magnitude > 5:
-      return "red";
-    case magnitude > 4:
-      return "orange";
-    case magnitude > 3:
-      return "yellow";
-    case magnitude > 2:
-      return "pink";
-    case magnitude > 1:
-      return "purple";
-    default:
-      return "green";
-    }
-  }
-  // set radiuss from magnitude
-    function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
-
-    return magnitude * 4;
-  }
-    // GeoJSON layer
-    L.geoJson(data, {
-      // Maken cricles
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-      },
-      // cirecle style
-      style: styleInfo,
-      // popup for each marker
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-      }
-    }).addTo(myMap);
-  
-    // an object legend
-    var legend = L.control({
-      position: "bottomright"
-    });
-  
-    // details for the legend
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-  
-      var earthquakeData = [0, 1, 2, 3, 4, 5];
-      var colors = [
-        "red",
-        "orange",
-        "yellow",
-        "pink",
-        "purple",
-        "green",
-      ];
-  
-      // Looping through
-      for (var i = 0; i < earthquakeData.length.length; i++) {
-       
-          "<i style='background: " + colors[i] + "'></i> " +
-         earthquakeData[i] + (earthquakeData[i + 1] + earthquakeData[i + 1] + "<br>");
-      }
-      return div;
     
-  
-    // Legend for map.
-    legend.addTo(myMap)
-    };
+    L.geoJSON(earthquakeData,{
+      pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, markerOptions);
+      }
+    })
+  }
+
+  // Create a GeoJSON layer 
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });  
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
